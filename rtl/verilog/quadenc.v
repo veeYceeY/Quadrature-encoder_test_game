1810b7e6852d65dd61b93f9322df02b4c301c226
veeyceey <veeyceey@gmail.com>
2017-01-21 23:05:45 -0800

    Cleaned quadenc.v

--- rtl/verilog/quadenc.v
+++ rtl/verilog/quadenc.v
@@ -32,27 +32,7 @@ module quadenc(
 	reg a_valid,b_valid;
 	reg [1:0] a_reg,b_reg;
 	wire clk_sample,clk_sys;
-	//reg clk;
-	//reg [32:0] count;
 	
-	
-	//clock devider
-	/*
-	always@(posedge(clk_in),negedge(rst_in)) begin
-		if(!rst_in) begin
-			clk<=0;
-			count<=18'b0;
-		end
-		else begin
-			if (count>10000) begin
-				clk<=!clk;
-			end
-			else begin
-				count<=count+1;
-			end
-		end
-	end
-	*/
 	assign clk_sample=clk_in;
 	assign clk_sys=clk_in;
 	
@@ -69,8 +49,6 @@ module quadenc(
 			a_buff[0]<=a_in;
 			b_buff[7:1]<=b_buff[6:0];
 			b_buff[0]<=b_in;
-			//a_valid<=a_valid;
-			//b_valid<=b_valid;
 			if(a_buff==8'b11111111) begin
 				a_valid<=1;
 			end
@@ -106,40 +84,5 @@ module quadenc(
 	
 	assign  cw_out = (a_reg==2'b00)&&(b_reg==2'b01);
 	assign acw_out = (a_reg==2'b01)&&(b_reg==2'b00);
-	/*
-	always@(posedge (clk_sys)) begin
-		if(!rst_in) begin
-			cw_pulse<=1'b0;
-			acw_pulse<=1'b0;
-		end
-		else begin
-			if(a_reg==2'b00) begin
-				if(b_reg==2'b01) begin
-					cw_pulse<=1;
-				end
-				else begin
-					cw_pulse<=0;
-				end
-				acw_pulse<=1'b0;
-			end
-			else begin
-				if(b_reg==2'b00) begin
-					if(a_reg==2'b01) begin
-						acw_pulse<=1;
-					end
-					else begin
-						acw_pulse<=0;
-					end
-					cw_pulse<=1'b1;
-				end
-				else begin
-					cw_pulse<=0;
-					acw_pulse<=0;
-				end
-			end
-		end
-	end
-	*/
-	//assign acw_out=acw_pulse;
-	//assign cw_out=cw_pulse;
+	
 endmodule
